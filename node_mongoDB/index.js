f87bf3ca07079c2aa482cb5c5f460f35602cbc64
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2021-08-11 17:29:23 +0530

    server migration
    success

--- node_mongoDB/index.js
+++ node_mongoDB/index.js
@@ -2,15 +2,26 @@ const connection = require("./model");
 const express = require("express");
 const application = express();
 const path = require("path");
-const ExpressHandlebars = require("express-handlebars");
+const expressHandlebars = require("express-handlebars");
 const bodyparser = require("body-parser");
 
 application.use(bodyparser.urlencoded({
     extended : true
 }));    
 
+application.set('views', path.join(__dirname, "/views/"));
+
+application.engine("hbs", expressHandlebars({
+    extname : "hbs", 
+    defaultLayout : "mainlayout",
+    layoutsDir : __dirname + "/views/layouts"
+}));
+
+application.set("view engine", "hbs" )
+
 application.get("/", (req, res)=>{
-    res.send('<h1> Hellow World <h1>')
+    //res.send('<h1> Hellow World <h1>')
+    res.render("index", {})
 })
 
 application.listen("3000", ()=> {
