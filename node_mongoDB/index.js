32046b799fdddd3fc706343cd52d78ce33944aa1
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2021-08-11 16:56:52 +0530

    Scheme added
    MongoDB schema added

--- node_mongoDB/index.js
+++ node_mongoDB/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/admin",{useNewUrlParser : true}, (error)=>{
     if(!error){
-        console.log("success");
+        console.log("Success Connected");
     }
     else{
         console.log("Error in connecting to database");
---
+++ node_mongoDB/model/marks.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+var MarkSchema = new mongoose.Schema({
+    name :{
+        type : String ,
+        required : "Required"
+    },
+    mark1 :{
+        type : Number ,
+        required : "Required"
+    },
+    mark2 :{
+        type : Number ,
+        required : "Required"
+    },
+    mark3 :{
+        type : Number ,
+        required : "Required"
+    },
+    mark4 :{
+        type : Number ,
+        required : "Required"
+    },
+    mark5 :{
+        type : Number ,
+        required : "Required"
+    },
+    mark6 :{
+        type : Number ,
+        required : "Required"
+    },
+})
