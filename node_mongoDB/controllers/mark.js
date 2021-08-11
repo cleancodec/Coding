9a9986e1d66ceb2c2dd59f9f4a7c4a5c84700232
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2021-08-11 20:27:38 +0530

    insertion completed

--- node_mongoDB/controllers/mark.js
+++ node_mongoDB/controllers/mark.js
@@ -9,6 +9,30 @@ router.get("/add", (req, res)=> {
     res.render("add-mark")
 })
 
+router.post("/add", (req, res)=> {
+    //res.render("add-mark");
+    //console.log(req.body);
+    var mark = new MarkModel();
+    mark.id = Math.ceil(Math.random() * 10000) + "";
+    mark.name = req.body.Sname;
+    mark.mark1 = req.body.m1;
+    mark.mark2 = req.body.m2;
+    mark.mark3 = req.body.m3;
+    mark.mark4 = req.body.m4;
+    mark.mark5 = req.body.m5;
+    mark.mark6 = req.body.m6;
+    mark.save((err, doc)=> {
+        if(!err)
+        {
+            res.redirect("/mark/list");
+        }
+        else
+        {
+            res.sent("Error occured");
+        }
+    })
+})
+
 router.get("/list", (req, res)=> {
 
     //getting data
