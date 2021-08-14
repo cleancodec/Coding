247c10e905404ca706d946f716b54c5ac644dddb
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2021-08-14 10:45:23 +0530

    Insertion Success
    Data Insertion Success

--- React/web app/client/src/App.js
+++ React/web app/client/src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+import Axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [mealName, setMealName] = useState('')
+  const [days, setDays] = useState(0)
+
+  const addToList = () =>{
+    //console.log(mealName + "   "+ days);
+    Axios.post("http://localhost:3001/insert" , {mealName : mealName , days : days});
+  }
+
   return (
     <div className="App">
-      
+      <h1>CURD APP</h1>
+        <label>Meal Name :</label>
+        <input type = "text" onChange = {(event) =>{setMealName(event.target.value);}}/>
+        <label>Days since order :</label>
+        <input type = "number" onChange = {(event) =>{setDays(event.target.value);}}/>
+        <button onClick = {addToList}>Add</button>
     </div>
   );
 }
--- React/web app/server/index.js
+++ React/web app/server/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Meals = require('./models/Meals');
+const cors = require('cors')
 const app  = express();
 
 const MealsModel = require("./models/Meals")
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://newuser:XTrDFPinesANPrc2@crud.dos9f.mongodb.net/meals?retryWrites=true&w=majority",{
     useNewUrlParser: true,
 });
 
-app.get('/', async (req, res) => {
+app.post('/insert', async (req, res) => {
     
-    const meals = new MealsModel({mealsName : "Apple", daysSinceIAte : 3});
+    const mealName = req.body.mealName
+    const days = req.body.days
+
+    const meals = new MealsModel({mealsName : mealName, daysSinceIAte : days});
     try {
         await meals.save();
     }catch(err)
