50ddc0eacaee1305a8aadba1a0177cde52d27e2c
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2021-08-14 09:30:19 +0530

    connection succeeded

--- React/web app/server/index.js
+++ React/web app/server/index.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app  = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Meals = require('./models/Meals');
+const app  = express();
+
+const MealsModel = require("./models/Meals")
 
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://newuser:XTrDFPinesANPrc2@crud.dos9f.mongodb.net/meals?retryWrites=true&w=majority",{
     useNewUrlParser: true,
 });
+
+app.get('/', async (req, res) => {
+    
+    const meals = new MealsModel({mealsName : "Apple", daysSinceIAte : 3});
+    try {
+        await meals.save();
+    }catch(err)
+    {
+        console.log(err);
+    }
+})
 
 app.listen(3001,()=>{
     console.log("Server running on  port 3001...");
---
+++ React/web app/server/models/Meals.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const MealsSchema = new mongoose.Schema({
+    mealsName : {
+        type : String,
+        required : true
+    },
+    daysSinceIAte : {
+        type : Number,
+        required : true
+    },
+});
+
+const Meals = mongoose.model("Meals", MealsSchema)
+module.exports = Meals
